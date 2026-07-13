8a6d6780ccd7f8ffd36afa46f895a9d8708be934
Connor Bowlan <connor.bowlan@project.com>
2026-07-13 14:15:02 -0700

    update to set correct pathing for deployed state

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://allisonhartpc.com',
+  site: 'https://connorbowlan.github.io',
+  base: '/allisonhartpc',
   integrations: [sitemap()],
 });
